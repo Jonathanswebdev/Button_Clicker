701ec6353131642169eef898e230f167f4ab4e27
Jonathan <121487539+Jonathanswebdev@users.noreply.github.com>
2023-03-21 10:20:50 -0700

    Button Clicker

---
+++ script.js
@@ -0,0 +1,15 @@
+// login button interaction
+function change(element) {
+  element.innerText = "Logout";
+}
+
+// 13 likes button interaction
+function likeButton() {
+  // we can include more code here if we want to
+  console.log("this message is coming from script.js");
+}
+
+// remove/hide button interaction
+function hide(element) {
+  element.remove();
+}
